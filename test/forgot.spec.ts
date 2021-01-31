1072acf1f9a2ef379bb31c66562cbf673878bc9c
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2021-01-31 17:26:41 -0300

    teste de forgot

---
+++ test/forgot.spec.ts
@@ -0,0 +1,27 @@
+import Token from 'App/Models/Token'
+import User from 'App/Models/User'
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = 'http://localhost:3333'
+
+let send = {
+  email: 'test@gmail.com',
+  password: '123456',
+  password_confirmation: '123456',
+}
+
+test.group('Forgot', () => {
+  /* test('forgot email', async (assert) => {
+    await supertest(BASE_URL).post('/forgot').expect(200).send({ email: send.email })
+    const user = await User.findByOrFail('email', send.email)
+
+    const token = await Token.findByOrFail('user_id', user.id)
+
+    assert.equal(token.userId, user.id)
+  }).timeout(10000) */
+
+  test('email not exist', async (assert) => {
+    await supertest(BASE_URL).post('/forgot').expect(409).send({ email: 'testqualquer@gmail.com' })
+  })
+})
