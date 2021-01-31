12213a1ed40e9756923d0a1744eee800e7a37a7d
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2021-01-31 20:29:22 -0300

    criaçao dos testes

---
+++ test/question.spec.ts
@@ -0,0 +1,77 @@
+import Exam from 'App/Models/Exam'
+import User from 'App/Models/User'
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = 'http://localhost:3333'
+
+let send = {
+  email: 'anotherTest2@gmail.com',
+  password: '123456',
+  password_confirmation: '123456',
+  first_name: 'Test',
+  last_name: 'Adonis',
+  phone: '(11)9xxxx-xxxx',
+  gender: 'Male',
+  birthday: '1989-06-26',
+  state: 'SP',
+  city: 'São Paulo',
+  is_teacher: 1,
+}
+
+test.group('Question', (group) => {
+  group.before(async () => {
+    const user = new User()
+    user.email = send.email
+    user.password = send.password
+    user.first_name = send.first_name
+    user.last_name = send.last_name
+    user.phone = send.phone
+    user.gender = send.gender
+    user.birthday = new Date()
+    user.state = send.state
+    user.city = send.city
+    user.is_teacher = true
+    user.admin = true
+    await user.save()
+  })
+
+  test('create a new Question', async (assert) => {
+    const login = await supertest(BASE_URL).post('/login').expect(200).send({
+      email: send.email,
+      password: send.password,
+    })
+
+    await supertest(BASE_URL)
+      .post('/newexam')
+      .expect(200)
+      .send({ exam: 'enem', location: 'BR' })
+      .set({ Authorization: `bearer ${login.body.token.token}` })
+
+    const responseQuestion = await supertest(BASE_URL)
+      .post('/newquetion')
+      .expect(200)
+      .field('enemArea', 'Ciencias Humanas')
+      .field('subjects', 'História')
+      .field('frente1', 'História do Brasil')
+      .field('frente2', 'Atualidade')
+      .field('frente3', 'Literatura')
+      .field('year', 2019)
+      .field('examId', 2)
+      .field('correct', 'A')
+      .attach('question', '/home/fernando/Documentos/ImageTest.png')
+      .set({ Authorization: `bearer ${login.body.token.token}` })
+
+    const selectQuestion = await supertest(BASE_URL)
+      .get(`/selectquestion/${responseQuestion.body.id}`)
+      .expect(200)
+      .set({ Authorization: `bearer ${login.body.token.token}` })
+
+    assert.equal(selectQuestion.body.subjects, 'História')
+
+    await supertest(BASE_URL)
+      .delete(`/deletequetion/${responseQuestion.body.id}`)
+      .expect(200)
+      .set({ Authorization: `bearer ${login.body.token.token}` })
+  })
+})
