8bb895b7e17cd6a32c063c8782521b1d3b3856cb
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2021-02-20 20:43:20 -0300

    criando teste simulados

---
+++ test/simulate.spec.ts
@@ -0,0 +1,32 @@
+import User from 'App/Models/User'
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = 'http://localhost:3333'
+
+let send = {
+  email: 'anotherTest2@gmail.com',
+  password: '123456',
+  password_confirmation: '123456',
+  first_name: 'Test',
+  last_name: 'Adonis',
+  phone: '(11)9xxxx-xxxx',
+  gender: 'Male',
+  birthday: '1989-06-26',
+  state: 'SP',
+  city: 'São Paulo',
+  is_teacher: 1,
+}
+
+test.group('Simulados', (group) => {
+  group.before(async () => {
+    const login = await supertest(BASE_URL).post('/login').expect(200).send({
+        email: send.email,
+        password: send.password,
+      })
+  })
+
+  test('2+2', async (assert) => {
+    assert.equal(2 + 2, 4)
+  })
+})
